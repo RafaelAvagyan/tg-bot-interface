d0b691f69fcbf3972332289abb6c2a44e9f92ef3
RafaelAvagyan <rafael.avagyan.97@mail.ru>
2025-04-17 15:26:23 +0300

    categories update

--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -1,5 +1,27 @@
 <script>
-
+    let categories = $state([
+        {id: 1, name: 'Дизайн'},
+        {id: 2, name: 'Доработки'},
+        {id: 3, name: 'Остальное'}
+    ])
 </script>
 
 <h1>Выберите категорию</h1>
+
+<div class="list">
+    {#each categories as item}
+        <div class="list-item">{item.name}</div>
+    {/each}
+</div>
+
+<style>
+  .list {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .list-item {
+    cursor: pointer;
+  }
+</style>
