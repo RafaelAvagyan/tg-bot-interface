e5aebe8c24ff67170d328c81bf857ee554bedceb
RafaelAvagyan <rafael.avagyan.97@mail.ru>
2025-04-17 18:03:38 +0300

    update

--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -1,17 +1,36 @@
 <script>
-    let categories = $state([
-        {id: 1, name: 'Дизайн'},
-        {id: 2, name: 'Доработки'},
-        {id: 3, name: 'Остальное'}
-    ])
+  import { page } from "$app/stores";
+
+  let taskId = $state();
+
+  taskId = $page.url.searchParams.get("taskId");
+
+  const handleCategoryClick = (category) => {
+    const tg = window.Telegram?.WebApp;
+
+    if (tg && taskId) {
+      tg.sendData(
+        JSON.stringify({
+          task_id: taskId,
+          category_id: category.id,
+        })
+      );
+      tg.close()
+    }
+  };
+  let categories = $state([
+    { id: 1, name: "Дизайн" },
+    { id: 2, name: "Доработки" },
+    { id: 3, name: "Остальное" },
+  ]);
 </script>
 
 <h1>Выберите категорию</h1>
 
 <div class="list">
-    {#each categories as item}
-        <div class="list-item">{item.name}</div>
-    {/each}
+  {#each categories as item}
+    <div on:click={() => handleCategoryClick(item)} class="list-item">{item.name}</div>
+  {/each}
 </div>
 
 <style>
