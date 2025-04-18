c1d8de9c7ff5215f8e692e0f5cb90b166bac24b0
RafaelAvagyan <rafael.avagyan.97@mail.ru>
2025-04-18 13:05:48 +0300

    update

--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -2,45 +2,35 @@
   import { onMount } from "svelte";
   import { supabase } from "$lib/supabaseClient";
   import { page } from "$app/stores";
+  import { get } from "svelte/store";
 
   let categories = $state([]);
   let isLoading = $state(true);
-  let taskId = $state(null);
-
-  onMount(() => {
-    taskId = $page.url.searchParams.get("taskId");
-  });
+  let taskId = null;
 
   const handleCategoryClick = async (item) => {
-    const tg = window.Telegram?.WebApp;
-
-    console.log(item, 'item')
-
-    if (tg && taskId && item.id) {
-      tg.sendData(
-        JSON.stringify({
-          task_id: taskId,
-          category_id: item.id,
-        })
-      );
-
-      const { data, error } = await supabase
-        .from("Todos")
-        .update({ categories_id: item.id })
-        .eq("id", taskId);
-
-      if (error) {
-        console.log("Ошибка todos", +error.message);
-      } else {
-        console.log("Задача обновлена", data);
-      }
-
-
-      tg.close();
+    if (!taskId) {
+      console.log("Нет taskId");
+      return;
     }
+
+    const { data, error } = await supabase
+      .from("Todos")
+      .update({ categories_id: item.id })
+      .eq("id", taskId);
+
+    if (error) {
+      console.log("Ошибка todos", +error.message);
+    } else {
+      console.log("Задача обновлена", data);
+    }
+
+    window.Telegram.WebApp.close();
   };
 
   onMount(async () => {
+    const url = get(page).url;
+    taskId = url.searchParams.get("taskId");
     const { data, error } = await supabase.from("Categories").select("*");
 
     if (error) {
@@ -57,7 +47,9 @@
 
 <div class="list">
   {#each categories as item}
-    <div on:click={() => handleCategoryClick(item)} class="item-list">{item.name}</div>
+    <div on:click={() => handleCategoryClick(item)} class="item-list">
+      {item.name}
+    </div>
   {/each}
 </div>
 
