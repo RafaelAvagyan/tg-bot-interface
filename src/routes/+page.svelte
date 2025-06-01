d139ef7e92c893e5a40276dfe0523e45a6bf7300
RafaelAvagyan <rafael.avagyan.97@mail.ru>
2025-06-01 20:41:12 +0300

    telegram fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,14 +74,20 @@
 </script>
 
 <div class="main">
-  {@html `<script
+  <!-- {@html `<script
     async
     src="https://telegram.org/js/telegram-widget.js?22"
     data-telegram-login="CreateTodoBot"
     data-size="large"
     data-auth-url="https://tg-bot-interface.vercel.app/"
     data-request-access="write"
-    ></script>`}
+    ></script>`} -->
+  {@html `<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="CreateTodoBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+<script type="text/javascript">
+  function onTelegramAuth(user) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
+</script>`}
   <h1>Главная</h1>
   {#if isLoading}
     <p>Загрузка...</p>
