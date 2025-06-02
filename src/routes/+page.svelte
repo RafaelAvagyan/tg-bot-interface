5081688d4bd3c06bbc1ce6420dac638109111c7d
RafaelAvagyan <rafael.avagyan.97@mail.ru>
2025-06-02 18:18:43 +0300

    fix tg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,20 +74,14 @@
 </script>
 
 <div class="main">
-  {@html `<script
-    async
-    src="https://telegram.org/js/telegram-widget.js?22"
-    data-telegram-login="CreateTodoBot"
-    data-size="large"
-    data-auth-url="https://tg-bot-interface.vercel.app/"
-    data-request-access="write"
-    ></script>`}
-  <!-- {@html `<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="CreateTodoBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-<script type="text/javascript">
-  function onTelegramAuth(user) {
-    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-  }
-</script>`} -->
+    {@html
+    `<script async src="https://telegram.org/js/telegram-widget.js?22"
+     data-telegram-login="CreateTodoBot" 
+     data-size="large" 
+     data-auth-url="https://tg-bot-interface.vercel.app" 
+     data-request-access="write">
+     </script>`
+    }
   <h1>Главная</h1>
   {#if isLoading}
     <p>Загрузка...</p>
